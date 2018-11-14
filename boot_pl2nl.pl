fa0b9bfdebc783e08512335085584576115683e2
CapelliC <cc.carlo.cap@gmail.com>
2018-11-14 14:29:55 +0100

    layout change

--- boot_pl2nl.pl
+++ boot_pl2nl.pl
@@ -5,9 +5,12 @@
     pseudo natural hhprolog loadable assembly
 */
 
-:- module(boot_pl2nl, [
-              boot_pl2nl/1, boot_pl2nl/2,
-              pl2nl//1, rule//1, callable//1,transl/2,tterm//3,targs//3
+:- module(boot_pl2nl,
+          [   boot_pl2nl/1
+             ,boot_pl2nl/2
+             ,tterm//3
+              %pl2nl//1, rule//1, callable//1,transl/2,
+              %targs//3
           ]).
 
 :- use_module(library(dcg/basics)).
@@ -33,17 +36,67 @@ tbody([],_,_) --> [].
 and(true) --> [].
 and(false) --> " and ".
 
-tterm(s(F,As),V0s,Vs) --> atom(F), " ", targs(As,V0s,Vs).
+%tterm(s(F,As), V0s,Vs) --> atom(F), " ", targs(As,V0s,Vs).
+% ?- S=s(add,[n(0),v('X'),v('X')]), phrase(tterm(S,[],Vs),T), format('<~s>~n', [T]).
+% ?- S=s(add,[s(s(s,[v('X')])),v('Y'),s(s(s,[v('Z')]))]), phrase(tterm(S,[],Vs),T), format('<~s>~n', [T]).
+tterm(s(F,As), V0s,Vs) -->
+    atom(F), " ",
+    targs(As,V0s,Vs,[],Bs),
+    deb(s(F,As)->Bs),
+    (   {Bs\=[]}
+    ->  {reverse(Bs,Cs)},
+        xargs(Cs)
+    ;   []
+    ).
 tterm(n(N), Vs,Vs) --> number(N).
 tterm(a(A), Vs,Vs) --> atom(A).
 tterm(v(V), Vs,Vs) --> atom(V).
 
 tterm(l(nil), Vs,Vs) --> "nil".
 tterm(l(H/nil), V0s,Vs) --> "lists ", tlist(H, V0s,Vs).
-tterm(l(H/T), V0s,Vs) --> "list ", tlist(H,V0s,V1s), tterm(T,V1s,Vs).
+tterm(l(H/T), V0s,Vs) --> deb(l(H/T)), "list ", tlist(H,V0s,V1s), tterm(T,V1s,Vs).
 
-targs([A|As], V0s,Vs) --> tterm(A,V0s,V1s), " ", targs(As,V1s,Vs).
+/*
+targs([A|As], V0s,Vs) --> deb(targs(A)), tfterm(A,V0s,V1s), " ", targs(As,V1s,Vs).
 targs([], Vs,Vs) --> [].
+*/
+targs([A|As], V0s,Vs, Bs,U) -->
+    deb(targs1(A)),
+    targ(A, V0s,V1s, Bs,U0), " ",
+    targs(As, V1s,Vs, U0,U),
+    deb(targs2(A,U)).
+targs([], Vs,Vs, S,S) --> [].
+
+targ(v(V), Vs,Vs, S,S) --> atom(V).
+targ(n(N), Vs,Vs, S,S) --> number(N).
+targ(s(Q), V0s,Vs, S,[V:s(F,As)|S]) -->
+    deb(+s(Q,Vs)),
+    {Q=..[F|As], Vs=[V|V0s]},
+    genvar(V0s,[V|V0s]), atom(V).
+targ(X, Vs,Vs, S,S) -->
+    deb(why(X)).
+
+genvar(V0s,[V|V0s]) -->
+    {length(V0s,N), format(atom(V),'_~d',[N])}.
+
+xargs([V:E|Es]) --> xarg(V,E), xargs(Es).
+xargs([]) --> [].
+
+xarg(V,E) --> " and ", atom(V), " holds ", xarg(E).
+
+xarg(s(F,_As)) --> atom(F).
+xarg(n(N)) --> number(N).
+xarg(v(V)) --> atom(V).
+
+tfterm(v(V), Vs,Vs) --> atom(V).
+tfterm(n(N), Vs,Vs) --> number(N).
+tfterm(l(H/T), V0s,Vs) -->
+    addvar(V0s,V1s), "list ", tlist(H,V0s,V1s), tterm(T,V1s,Vs).
+
+addvar(V0s,[V|V0s]) -->
+    {length(V0s,N), format(atom(V),'_~d',[N])},
+    atom(V), " and ", atom(V), " holds ", deb(addvar(V0s, V)).
+
 
 tlist([], Vs,Vs) --> [].
 tlist([H|T], V0s,Vs) --> tterm(H,V0s,V1s), " ", tlist(T,V1s,Vs).
@@ -88,19 +141,18 @@ list(nil) --> s, "[", s, "]".
 funct(F) --> name(F).
 
 name(N) -->
-    ctype([csymf, lower], C),
-    cstype(csym, Cs),
+    typesc([csymf,lower], C),
+    typescs([csym], Cs),
     {atom_codes(N,[C|Cs])}.
 
 var(V) -->
-    ctype(prolog_var_start,C),
-    cstype(prolog_identifier_continue, Cs),
+    typesc([prolog_var_start],C),
+    typescs([prolog_identifier_continue], Cs),
     {atom_codes(V,[C|Cs])}.
 
-ctype(T, C) --> [C], {is_list(T) -> maplist(code_type(C),T) ; code_type(C,T)}.
-
-cstype(Type, [C|Cs]) --> [C], {code_type(C,Type)}, cstype(Type, Cs).
-cstype(_, []) --> [].
+typesc(T, C) --> [C], {maplist(code_type(C),T)}.
+typescs(T, [C|Cs]) --> typesc(T,C), typescs(T,Cs).
+typescs(_, []) --> [].
 
 % debug
 deb(X) --> {writeln(+X)}.
